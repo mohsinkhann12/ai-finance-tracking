853122441b58c699411884c9c98cc34627bdc223
Mohsin Khan <87082611+mohsinkhann12@users.noreply.github.com>
2024-10-29 00:27:40 +0530

    Update dbConfig.jsx

--- utils/dbConfig.jsx
+++ utils/dbConfig.jsx
@@ -2,6 +2,6 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 const sql = neon(
-  "postgresql://finan-smart_owner:uk3aed9QZotj@ep-wispy-breeze-a5iadk8t.us-east-2.aws.neon.tech/finan-smart?sslmode=require"
+  "postgresql://neondb_owner:QPT2x8mFLdBN@ep-restless-glitter-a81n4uy0.eastus2.azure.neon.tech/neondb?sslmode=require"
 );
 export const db = drizzle(sql, { schema });
